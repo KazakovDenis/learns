0d86c9923bb9ea4cd019f0883627cf5d2eea94b0
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-09-10 22:53:09 +0700

    basic examples

--- technologies/rabbitmq/consumer.py
+++ technologies/rabbitmq/consumer.py
@@ -1,13 +1,8 @@
 """
-Hello-world пример для работы с RabbitMQ.
+'Hello world' пример для работы с RabbitMQ.
 Consumer - подписчик на сообщения.
 """
-from logging import Logger, StreamHandler
-from sys import stdout
-
 from pika import BlockingConnection, ConnectionParameters
-
-from .common import RabbitMQMixin
 
 
 def receive_msg_example(queue: str):
@@ -60,33 +55,5 @@ def receive_msg_example(queue: str):
         exit(0)
 
 
-# Ниже пример на приложении:
-class LogService(RabbitMQMixin):
-    """Сервис, имитирующий подписчика на сообщения"""
-
-    def __init__(self):
-        self.log = Logger(self.__class__.__name__)
-        handler = StreamHandler(stdout)
-        handler.setLevel(10)
-        self.log.addHandler(handler)
-
-        RabbitMQMixin.__init__(self)
-        self.consume_from_rabbit(self.on_message)
-
-    def on_message(self, ch, method, properties, body):
-        """Функция для обработки сообщения из очереди RabbitMQ"""
-        msg = f'[LogService] [{method.routing_key}] GET request from {body.decode()} '
-        self.log.info(msg)
-
-    def run(self):
-        try:
-            print('[LogService] Service started')
-            self.rabbit.start_consuming()
-        finally:
-            print('[LogService] Shutting down...')
-            self.rabbit.stop_consuming()
-            exit(0)
-
-
 if __name__ == '__main__':
-    LogService().run()
+    receive_msg_example('test')
--- technologies/rabbitmq/producer.py
+++ technologies/rabbitmq/producer.py
@@ -1,13 +1,8 @@
 """
-Hello-world пример для работы с RabbitMQ.
+'Hello world' пример для работы с RabbitMQ.
 Producer - отправитель сообщения.
 """
-from datetime import datetime
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
 from pika import BlockingConnection, BasicProperties, ConnectionParameters
-
-from .common import RabbitMQMixin
 
 
 def send_msg_example(queue: str, message: str):
@@ -22,10 +17,16 @@ def send_msg_example(queue: str, message: str):
     channel = connection.channel()
 
     # проверяем, что очередь сущетсвует, или создаем новую
+    # method = channel.queue_declare('') создаст временную очередь со случайным именем
     channel.queue_declare(
         queue=queue,                    # название
         durable=True,                   # объявить устойчивой
     )
+
+    # необязательно: создаём обменник и связываем с очередью
+    # channel.exchange_declare('logs', exchange_type='fanout')
+    # channel.queue_bind(method.method.queue, 'logs')
+    # с типом fanout при отправке сообщения routing_key можно не указывать
 
     # отправляем сообщение
     channel.basic_publish(
@@ -39,39 +40,5 @@ def send_msg_example(queue: str, message: str):
     connection.close()
 
 
-# Ниже пример на приложении:
-class HTTPRequestHandler(BaseHTTPRequestHandler):
-    """Обработчик HTTP-запроса.
-    Использует метод send_to_rabbit миксина сервера для отправки сообщения в очередь.
-    """
-
-    def do_GET(self):
-        """На GET-запрос возвращаем текущее время и отправляем в очередь адрес клиента"""
-        self.send_response(200)
-        self.end_headers()
-        now = datetime.now().isoformat(timespec='seconds').encode()
-        self.wfile.write(b'<h1>This request\'s time: %s</h1>' % now)
-        self.server.send_to_rabbit(self.address_string())
-
-
-class AppServer(HTTPServer, RabbitMQMixin):
-    """Сервер, имитирующий реальное приложение"""
-
-    def __init__(self, host, port):
-        super().__init__((host, port), HTTPRequestHandler)
-        RabbitMQMixin.__init__(self)
-
-    def run(self):
-        host, port = self.server_address[:2]
-        try:
-            print(f'[AppServer] Serving HTTP on {host} port {port}\n')
-            self.serve_forever()
-        except KeyboardInterrupt:
-            print('[AppServer] Shutting down...')
-        finally:
-            self.rabbit.close()
-            self.server_close()
-
-
 if __name__ == '__main__':
-    AppServer('localhost', 8000).run()
+    send_msg_example('test', 'Hello world!')
