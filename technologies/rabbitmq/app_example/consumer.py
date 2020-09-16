77da5b77e9ddfe5eda734efb19c6dbf5216f8f18
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-09-16 11:06:07 +0700

    LogService subscribe direct

--- technologies/rabbitmq/app_example/consumer.py
+++ technologies/rabbitmq/app_example/consumer.py
@@ -5,43 +5,35 @@ from logging import getLogger, Logger, StreamHandler
 from sys import stdout
 from threading import Thread, Event
 
-from .common import RabbitMQMixin
+from .common import RabbitMQMixin, EXCHANGE, EX_TYPE
 
 
 class LogService(Thread, RabbitMQMixin):
     """Сервис, имитирующий подписчика на сообщения"""
 
     instances = 0
+    xch_name = EXCHANGE
+    xch_type = EX_TYPE
 
     def __init__(self, rabbit_key: str):
         Thread.__init__(self, daemon=True)
         LogService.instances += 1
-
         self.number = LogService.instances
         self.log = self.get_logger()
 
-        self.rabbit = self.connect_to_rabbit()
-        self.consume_from_rabbit(self.on_message, rabbit_key)
+        self.rabbit = self.connect_to_rabbit(self.xch_name, self.xch_type)
+        self.subscribe(self.xch_name, rabbit_key)
 
     def get_logger(self) -> Logger:
         logger = getLogger(str(self))
         handler = StreamHandler(stdout)
-        handler.setLevel(10)
         logger.addHandler(handler)
+        logger.setLevel(10)
+        handler.setLevel(10)
         return logger
 
-    def declare(self, *routing_keys: str, exchange: str = ''):
-        """Подключаемся к RabbitMQ и создаём очередь"""
-        # if exchange:
-        #     self.rabbit.exchange_declare(exchange, exchange_type='topic')
-
-        for key in routing_keys:
-            res = self.rabbit.queue_declare(key, durable=True)
-            # if exchange:
-            #     self.rabbit.queue_bind(res.method.queue, exchange, routing_key=key)
-
     def on_message(self, ch, method, properties, body):
-        """Функция для обработки сообщения из очереди RabbitMQ"""
+        """Метод для обработки сообщения из очереди RabbitMQ"""
         remote, path = body.decode().split()
         msg = f'[{self}] [{method.routing_key}] GET request "{path}" from {remote} '
         self.log.info(msg)
@@ -58,19 +50,20 @@ class LogService(Thread, RabbitMQMixin):
 def run_many(*routing_keys):
     """Запускает несколько экземпляров LogService"""
     threads = [LogService(key) for key in routing_keys]
+    event = Event()
 
     for thread in threads:
         thread.start()
 
     try:
-        Event().wait()
+        event.wait()
     except KeyboardInterrupt:
-        raise SystemExit
+        event.set()
 
 
 if __name__ == '__main__':
     run_many(
         'app.index',     # забирает логи только главной страницы
         'app.other',     # забирает логи остальных страниц
-        'app.*',         # забирает логи всех страниц
+        'index',         # забирает логи всех страниц
     )
