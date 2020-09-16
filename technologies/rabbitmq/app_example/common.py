e45b8d10c4fb6c6ae3259d750c8595c69ebf4b25
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-09-16 10:39:54 +0700

    RabbitMQMixin subscribe

--- technologies/rabbitmq/app_example/common.py
+++ technologies/rabbitmq/app_example/common.py
@@ -1,10 +1,14 @@
 """
 Модуль с общей логикой работы с RabbitMQ
 """
-from typing import Callable, Union
+from typing import Union
 
 from pika import BlockingConnection, ConnectionParameters, BasicProperties
 from pika.adapters.blocking_connection import BlockingChannel
+
+
+EXCHANGE = 'app'
+EX_TYPE = 'direct'
 
 
 class RabbitMQMixin:
@@ -13,10 +17,16 @@ class RabbitMQMixin:
     rabbit: BlockingChannel
 
     @staticmethod
-    def connect_to_rabbit():
-        """Подключаемся к RabbitMQ и создаём очередь"""
+    def connect_to_rabbit(exchange_name: str = '', exchange_type: str = ''):
+        """Подключаемся к RabbitMQ и создаём обменник
+
+        :param exchange_name: название обменника
+        :param exchange_type: тип обменника
+        """
         connection = BlockingConnection(ConnectionParameters())
-        return connection.channel()
+        channel = connection.channel()
+        channel.exchange_declare(exchange_name, exchange_type)
+        return channel
 
     def send_to_rabbit(self, message: Union[str, bytes], exchange: str = '', queue: str = ''):
         """Отправляем сообщение в очередь
@@ -32,15 +42,24 @@ class RabbitMQMixin:
             properties=BasicProperties(delivery_mode=2),
         )
 
-    def consume_from_rabbit(self, callback: Callable, queue: str = ''):
-        """Подписываемся на сообщения очереди
+    def subscribe(self, exchange: str = '', *routing_keys: str):
+        """Завязываем очереди на обменник и подписываемся
 
-        :param callback: функция для обработки сообщения
-        :param queue: название очереди
+        :param exchange: название обменника
+        :param routing_keys: ключи для привязки очередей
         """
-        self.rabbit.basic_qos(prefetch_count=1)
+        m = self.rabbit.queue_declare('', durable=True)
+        queue = m.method.queue
+
+        for key in routing_keys:
+            self.rabbit.queue_bind(queue, exchange, key)
+
         self.rabbit.basic_consume(
             queue=queue,
-            on_message_callback=callback,
+            on_message_callback=self.on_message,
             auto_ack=True,
         )
+
+    def on_message(self, *args):
+        """Метод для обработки сообщения из очереди RabbitMQ"""
+        pass
