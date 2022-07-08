e770f91d9377b2d5b711a069b48418e289855c6f
Denis Kazakov <denis@kazakov.ru.net>
2022-07-09 00:00:08 +0300

    Go: dependencies & code style

---
+++ languages/Go/src/main/13_code_style.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+Форматирование:
+- https://golang.org/cmd/gofmt/
+- https://pkg.go.dev/golang.org/x/tools/cmd/goimports
+
+Линтеры:
+- https://golang.org/cmd/vet/
+
+Документирование:
+- https://pkg.go.dev/golang.org/x/tools/cmd/godoc
+
+Документирование происходит на основе комментариев.
+*/
